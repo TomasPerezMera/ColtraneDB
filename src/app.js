b298948a968146a355f36981b70206452ba3397b
Tomás Pérez Mera <tperezmera@gmail.com>
2026-02-24 19:21:07 +0100

    Created utils.js, /views directory, basic Server.io configuration.-

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 require('dotenv').config();
 import express from 'express';
-import userRouter from './routes/user.router.js';
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars';
+import viewsRouter from './routes/views.router.js';
+import {Server} from 'socket.io';
 import mongoose from 'mongoose';
+import { userRouter } from './routes/user.router.js';
+
 
 const app = express();
-const server = app.listen(8080, () => console.log('Server is running on port 8080'));
+const httpServer = app.listen(8080, () => console.log('Listening on Port 8080'));
+
+const io = new Server(httpServer);
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+app.use('/', viewsRouter);
+app.use('/api/users', userRouter);
+
+
 
 mongoose.connect(process.env.MONGO_URI, (error) => {
     if (error) {
@@ -13,4 +30,3 @@ mongoose.connect(process.env.MONGO_URI, (error) => {
     }
 });
 
-app.use('/api/users', userRouter);
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,1 +1,9 @@
 import mongoose from 'mongoose';
+
+const userCollection = 'users';
+
+const userSchema = new mongoose.Schema({
+    name: String,
+});
+
+export const userRouter = mongoose.model(userCollection, userSchema);
